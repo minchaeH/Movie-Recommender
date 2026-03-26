85f0b9db1a65c2db73afd532f28c3b1982f88aad
minchaeH <bunnycap2006@gmail.com>
2026-03-26 13:56:47 +0900

    최종 과제 제출

--- Rating.cpp
+++ Rating.cpp
@@ -5,8 +5,10 @@ Rating::Rating() : userId(0), movieId(0), score(0) {}
 Rating::Rating(int userId, int movieId, double score)
     : userId(userId), movieId(movieId), score(score) {}
 
+
 int Rating::getUserId() const { return userId; }
 int Rating::getMovieId() const { return movieId; }
+
 double Rating::getScore() const { 
     if (score < 0.0 || score > 5.0) {
         return 0.0;
@@ -15,6 +17,12 @@ double Rating::getScore() const {
 }
 
 void Rating::display() const {
-    std::cout << "User ID: " << userId << ", Movie ID: " << movieId << ", Score: " << score << std::endl;
+    std::cout << "User ID: " << userId << ", Movie ID: " << movieId << ", Score: ";
+    if (score < 0.0 || score > 5.0) {
+        std::cout << 0.0; } 
+    else {
+        std::cout << score; }
+    std::cout << std::endl;
+    
 }
 
